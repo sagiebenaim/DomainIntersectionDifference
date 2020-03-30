13a492d72bbeb1471b158a6488505d50ed718b49
sagiebenaim <sagiebenaim@gmail.com>
2020-03-30 21:04:38 +0300

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -159,10 +159,10 @@ def preprocess_folders(args):
     if not os.path.exists(args.dest):
         os.mkdir(args.dest)
 
-    trainA = os.listdir(os.path.join(args.root), 'trainA')
-    trainB = os.listdir(os.path.join(args.root), 'trainB')
-    testA = os.listdir(os.path.join(args.root), 'testA')
-    testB = os.listdir(os.path.join(args.root), 'testB')
+    trainA = os.listdir(os.path.join(args.root, 'trainA'))
+    trainB = os.listdir(os.path.join(args.root, 'trainB'))
+    testA = os.listdir(os.path.join(args.root, 'testA'))
+    testB = os.listdir(os.path.join(args.root, 'testB'))
 
     with open(os.path.join(args.dest, 'testA.txt'), 'w') as f:
         for i, _img in enumerate(testA):
